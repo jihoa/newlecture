1fcd928d3c21090346099a78c10aeaf507ddb98b
COMP1-PC\cph <jiho.develop@gmail.com>
2021-03-28 22:07:20 +0900

    first

---
+++ src/com/newlecture/web/controller/NoticeListController.java
@@ -0,0 +1,31 @@
+package com.newlecture.web.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
+
+@WebServlet("/notice/list")
+public class NoticeListController extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		NoticeService service = new NoticeService();
+		
+		List<Notice> list = service.getNoticeList();
+			
+		request.setAttribute("list", list);
+		
+		request
+		.getRequestDispatcher("/notice/list.jsp")
+		.forward(request, response);
+		
+	}
+}
