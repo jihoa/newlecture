88ac2d777e6cec481e7b62479137f55c7d0a20e8
COMP1-PC\cph <jiho.develop@gmail.com>
2021-07-06 10:05:55 +0900

    github

--- src/com/newlecture/web/controller/NoticeDetailController.java
+++ src/com/newlecture/web/controller/NoticeDetailController.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.newlecture.web.controller;
 
 import java.io.IOException;
@@ -9,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,53 +16,53 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.newlecture.web.entity.Notice;
 
-/**
- * @author jiho-kim
- *
- * 신규개발
- */
-
 @WebServlet("/notice/detail")
-public class NoticeDetailController extends HttpServlet{
-
+public class NoticeDetailController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		int id = Integer.parseInt(request.getParameter("id"));
-		
-		String url = "jdbc:oracle:thin:@10.10.0.131:1521:M2";
-		String sql = "SELECT * FROM NOTICE1 where id = ? ";
+	    
+		String url = "jdbc:oracle:thin:@192.168.77.1:1521/xepdb1";
+		String sql = "SELECT * FROM NOTICE WHERE ID=?";
 
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
-			
-			Connection con = DriverManager.getConnection(url, "cli", "cli1993");
+			Connection con= DriverManager.getConnection(url,"NEWLEC","1234");
 			PreparedStatement st = con.prepareStatement(sql);
-			
-			st.setInt(1, id);
-			ResultSet rs = st.executeQuery();
-			
-			
+			st.setInt(1,id);
+			ResultSet rs= st.executeQuery();
+
 			rs.next();
-			
-			String title = rs.getString("TITLE");
-			
-			
-			
-			Notice notice = new Notice( id , title);
-			
+			String title=rs.getString("TITLE");
+			String writerId=rs.getString("WRITER_ID");
+			Date regdate=rs.getDate("REGDATE"); 
+			String hit=rs.getString("HIT");
+			String files=rs.getString("FILES");
+			String content=rs.getString("CONTENT");  
+
+//			request.setAttribute("title", title);
+//			request.setAttribute("writerId", writerId);
+//			request.setAttribute("regdate", regdate);
+//			request.setAttribute("hit", hit);
+//			request.setAttribute("files", files);
+//			request.setAttribute("content", content);
+
+			Notice notice= new Notice(
+					id,
+					title,
+					writerId,
+					regdate,
+					hit,
+					files,
+					content
+					);
 			
 			request.setAttribute("n", notice);
-
-			//request.setAttribute("title", title);
+			
 			
 			rs.close();
 			st.close();
-			con.close();						
-			
-			
-			
+			con.close();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -73,13 +71,7 @@ public class NoticeDetailController extends HttpServlet{
 			e.printStackTrace();
 		}
 		
-		
-		
-		
-		//redirect
-		//forward 전이방식.
-		request.getRequestDispatcher("/notice/detail.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/view/notice/detail.jsp").forward(request, response);
 		
 	}
-
 }
