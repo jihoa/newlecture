2fed6fd78071cd7ca97ea62960287f95aa9ca639
COMP1-PC\cph <jiho.develop@gmail.com>
2021-07-07 17:37:08 +0900

    top down

---
+++ src/com/newlecture/web/service/NoticeService.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.newlecture.web.service;
+
+import java.util.List;
+
+import com.newlecture.web.entity.Notice;
+
+/**
+ * @author jiho-kim
+ *
+ * 신규개발
+ */
+public class NoticeService {
+	public List<Notice> getNoticeList(){
+		return getNoticeList("title", "", 1);
+	}
+	public List<Notice> getNoticeList(int page){
+		return getNoticeList("title", "", page);
+	}
+	public List<Notice> getNoticeList(String field, String query, int page){
+		return null;
+	}	
+	
+	public int getNoticeCount() {
+		return 0;
+	}
+	
+	public int getNoticeCount(String field, String query) {
+		return 0;
+	}
+	
+	public Notice getNotice(int id) {
+		String sql = "select * from notice1 where id = ? ";
+		return null;
+	}
+	
+	public Notice getNextNotice(int id) {
+		return null;
+	}
+	
+	public Notice getPrevNotice(int id) {
+		return null;
+	}	
+	
+}
