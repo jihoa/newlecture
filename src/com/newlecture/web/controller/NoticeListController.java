2e8bb09c7f34125a97d3abfe171bf120fcb5bc79
COMP1-PC\cph <jiho.develop@gmail.com>
2021-07-08 13:31:31 +0900

    tt

--- src/com/newlecture/web/controller/NoticeListController.java
+++ src/com/newlecture/web/controller/NoticeListController.java
@@ -17,53 +17,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 @WebServlet("/notice/list")
 public class NoticeListController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//list?f=title&q=a
+		String field = request.getParameter("f");
+		String query = request.getParameter("q");
 		
-		List<Notice> list = new ArrayList<>();
+		NoticeService service = new NoticeService();
 		
-		String url = "jdbc:oracle:thin:@10.10.0.131:1521:M2";
-		String sql = "SELECT * FROM NOTICE1";
+		//List<Notice> list = new ArrayList<>();
+		List<Notice> list = service.getNoticeList(field, query, 1);
 
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con= DriverManager.getConnection(url, "cli", "cli1993");
-			Statement st = con.createStatement();
-			ResultSet rs= st.executeQuery(sql);
-
-			 while(rs.next()){ 
-			 	int id = rs.getInt("ID");
-			 	String title=rs.getString("TITLE");
-				String writerId=rs.getString("WRITER_ID");
-				Date regdate=rs.getDate("REGDATE"); 
-				String hit=rs.getString("HIT");
-				String files=rs.getString("FILES");
-				String content=rs.getString("CONTENT"); 
-
-				Notice notice= new Notice(
-						id,
-						title,
-						writerId,
-						regdate,
-						hit,
-						files,
-						content
-						);
-				list.add(notice);
-			} 
-			 	rs.close();
-				st.close();
-				con.close();
-
-			
-			
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		request.setAttribute("list", list);
 		
 		request
--- src/com/newlecture/web/service/NoticeService.java
+++ src/com/newlecture/web/service/NoticeService.java
@@ -3,6 +3,14 @@
  */
 package com.newlecture.web.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.newlecture.web.entity.Notice;
@@ -20,15 +28,89 @@ public class NoticeService {
 		return getNoticeList("title", "", page);
 	}
 	public List<Notice> getNoticeList(String field, String query, int page){
-		return null;
+
+		List<Notice> list = new ArrayList<>();
+		
+		String sql="";
+
+		
+		String url = "jdbc:oracle:thin:@10.10.0.131:1521:M2";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con= DriverManager.getConnection(url, "cli", "cli1993");
+			PreparedStatement st = con.prepareStatement(sql);
+			ResultSet rs= st.executeQuery();
+
+			 while(rs.next()){ 
+			 	int id = rs.getInt("ID");
+			 	String title=rs.getString("TITLE");
+				String writerId=rs.getString("WRITER_ID");
+				Date regdate=rs.getDate("REGDATE"); 
+				String hit=rs.getString("HIT");
+				String files=rs.getString("FILES");
+				String content=rs.getString("CONTENT"); 
+
+				Notice notice= new Notice(
+						id,
+						title,
+						writerId,
+						regdate,
+						hit,
+						files,
+						content
+						);
+				list.add(notice);
+			} 
+			 	rs.close();
+				st.close();
+				con.close();
+
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return list;
 	}	
 	
 	public int getNoticeCount() {
-		return 0;
+		return getNoticeCount("title", "");
 	}
 	
 	public int getNoticeCount(String field, String query) {
-		return 0;
+		
+		int count = 0;
+		
+		String sql = "";
+
+		String url = "jdbc:oracle:thin:@10.10.0.131:1521:M2";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con= DriverManager.getConnection(url, "cli", "cli1993");
+			PreparedStatement st = con.prepareStatement(sql);
+			ResultSet rs= st.executeQuery();
+
+			count = rs.getInt("count");
+			
+			 	rs.close();
+				st.close();
+				con.close();
+
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		return count;
 	}
 	
 	public Notice getNotice(int id) {
@@ -37,10 +119,13 @@ public class NoticeService {
 	}
 	
 	public Notice getNextNotice(int id) {
-		return null;
+		Notice notice = null;
+		
+		return notice;
 	}
 	
 	public Notice getPrevNotice(int id) {
+		Notice notice = null;
 		return null;
 	}	
 	
